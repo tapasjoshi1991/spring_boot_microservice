470a9a7d07f95cb51e199ec9edea27986afb25fe
Admin <Admin@NODE812>
2018-02-26 12:38:23 +0530

    committing the changes for discovery_server

---
+++ src/main/java/com/eureka/util/DiscoveryServerApplication.java
@@ -0,0 +1,17 @@
+package com.eureka.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting the Discovery server");
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+	
+	
+}
